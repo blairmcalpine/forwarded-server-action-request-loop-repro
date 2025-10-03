98b7e13529fe20d70f09a1b0bf52c5d75e2c6cf1
Blair McAlpine <blair.mcalpine@faire.com>
2025-10-03 16:39:08 -0400

    Remove unneeded stuff

--- src/app/rewrite/button.tsx
+++ src/app/rewrite/button.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useContext } from "react";
-import { action, redirectAction } from "./actions";
+import { action } from "./actions";
 import { DataContext } from "./context";
 
 export function Button() {
@@ -14,22 +14,10 @@ export function Button() {
     setData(result);
   };
 
-  const handleRedirect = async () => {
-    await new Promise((res) => setTimeout(res, 1000));
-
-    const result = await redirectAction();
-
-    setData(result);
-  };
-
   return (
     <>
       <button onClick={handleClick} id="run-action">
         Run Action
-      </button>
-
-      <button onClick={handleRedirect} id="run-action-redirect">
-        Run Redirect
       </button>
     </>
   );
